ac3dc9d1740220d8b5a3564b79166edecb8658f6
Beatriz Mendes <bea.smendes98@gmail.com>
2023-03-25 13:34:45 +0100

    chore: add server

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 8080; // default port to listen
+
+app.use(cors());
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
