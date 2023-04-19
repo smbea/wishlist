16c48068013c55c4542828a04e6f8278b60bbf5d
Beatriz Mendes <bea.smendes98@gmail.com>
2023-04-19 22:26:08 +0200

    feat: add User models

---
+++ server/src/db.ts
@@ -0,0 +1,30 @@
+import { Sequelize } from 'sequelize-typescript';
+import { User } from './models/User';
+
+let sequelize;
+
+export const connectToDb = async () => {
+
+  sequelize = new Sequelize({
+    database: 'postgres',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'postgres',
+    storage: ':memory:',
+    host: '0.0.0.0',
+    models: [User]
+  });
+
+  try {
+     await sequelize.authenticate();
+     console.log('Connection has been established successfully.');
+
+     await User.sync();
+     await User.create({username: "user", password: "user"});
+
+    return sequelize;
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,38 @@
 import express from "express";
-import {connectToDb} from "./queries";
+import {connectToDb} from "./db";
 import cors from "cors";
+import bodyParser from "body-parser";
+import {User} from "./models/User";
+import { Sequelize } from "sequelize";
+
 
 const app = express();
 const port = 8080; // default port to listen
 
-// connectToDb()
+const db:Sequelize = null;
+
+connectToDb()
 
 app.use(cors());
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
+} );
+
+app.post( "/login", async ( req, res ) => {
+  const {username, password} = req.body;
+
+  const user = await User.findOne({where: {username, password}});
+
+  if(user) {
+    res.status(200).send({message: "ok"})
+  } else {
+    res.status(409).send({message: "The credentials are not correct"})
+  }
 } );
 
 app.get( "/item", ( req, res ) => {
