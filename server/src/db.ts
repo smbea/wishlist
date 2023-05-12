432575e9e60fa2672a1e6e834b0c1c706c7d0c57
Beatriz Mendes <bea.smendes98@gmail.com>
2023-05-12 22:16:10 +0100

    chore(server): fix linting errors

--- server/src/db.ts
+++ server/src/db.ts
@@ -4,34 +4,30 @@ import { Item } from './models/Item';
 
 let sequelize;
 
-export const connectToDb = async () => {
-
+export const connectToDb = async (): Promise<Sequelize> => {
   sequelize = new Sequelize({
     database: 'postgres',
     dialect: 'postgres',
     username: 'postgres',
     password: 'postgres',
     host: 'postgres',
-    models: [User, Item]
+    models: [ User, Item ]
   });
 
   try {
-
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
 
     await User.sync();
-    const users =  await User.findAll();
+    const users = await User.findAll();
 
-    if(users.length === 0)
-      await User.create({username: "user", password: "user"});
+    if (users.length === 0) { await User.create({ username: 'user', password: 'user' }); }
 
     return sequelize;
-
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
-}
+};
 
 module.exports = {
   connectToDb
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,80 +1,85 @@
-import express from "express";
-import {connectToDb} from "./db";
-import cors from "cors";
-import bodyParser from "body-parser";
-import {User} from "./models/User";
-import { Sequelize } from "sequelize";
-import { Item } from "./models/Item";
-
+import express from 'express';
+import { connectToDb } from './db';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { User } from './models/User';
+import { Item } from './models/Item';
 
 const app = express();
 const port = 8080; // default port to listen
 
-const db: typeof Sequelize = null;
-
-connectToDb()
+connectToDb().catch(e => {
+  console.log(e);
+});
 
 app.use(cors());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
-app.post( "/login", async ( req, res ) => {
-  const {username, password} = req.body;
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
 
-  const user = await User.findOne({where: {username, password}});
+  const user = await User.findOne({ where: { username, password } });
 
-  if(user) {
-    res.status(200).send({message: "okk"})
+  if (user != null) {
+    res.status(200).send({ message: 'okk' });
   } else {
-    res.status(409).send({message: "The credentials are not correct"})
+    res.status(409).send({ message: 'The credentials are not correct' });
   }
-} );
+});
 
-
-app.post( "/item", async ( req, res ) => {
+app.post('/item', async (req, res) => {
   const data = req.body;
 
   await Item.sync();
-  await Item.create({...data});
+  await Item.create({ ...data });
 
-  res.status(200).send({message: "ok"});
-} );
+  res.status(200).send({ message: 'ok' });
+});
 
+app.get('/item', (req, res) => {
+  const url: string = req.query.foo as string;
 
-app.get( "/item", ( req, res ) => {
-  fetch(`http://127.0.0.1:8000/?url=${req.query.url}`, {headers: {'Content-type': 'application/json'}})
-      .then(async (response) => {
-        // console.log(response)
+  fetch(`http://127.0.0.1:8000/?url=${url}`, { headers: { 'Content-type': 'application/json' } })
+    .then(async (response) => {
+      if (response.ok) {
         const body = await response.json();
-        res.send(body);
-      })
-      .catch(error => {
-        console.log(error)
-        // res.status(500).send(error)
-      });
+        res.status(200).send(body);
+      } else {
+        const error = await response.json();
+        res.status(400).send({ error });
+      }
+    }).catch((e) => {
+      console.log(e);
+    });
+});
 
-} );
-
-
-app.get( "/items", async ( req, res ) => {
+app.get('/items', async (req, res) => {
   const items = await Item.findAll();
-  res.send(items)
-} );
 
-app.delete( "/item", async ( req, res ) => {
+  res.send(items);
+});
+
+app.delete('/item', async (req, res) => {
   const id = req.body.id;
 
-  Item.destroy({where: {id}})
-} );
+  Item.destroy({ where: { id } })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.sendStatus(400);
+    });
+});
 
 // start the Express server
 app.listen(port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+  console.log(`server started at http://localhost:${port}`);
+});
