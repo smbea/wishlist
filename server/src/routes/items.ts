a5e80c63432b8b08af44c5f20bdcc5d6de04ee3b
Beatriz Mendes <bea.smendes98@gmail.com>
2023-07-01 22:44:36 +0200

    chore(server): clean up routes

--- server/src/routes/items.ts
+++ server/src/routes/items.ts
@@ -3,6 +3,33 @@ import { Item } from '../models/Item';
 
 const router: express.Router = express.Router({ mergeParams: true });
 
+// gets
+router.get('/', async (req, res) => {
+  const items = await Item.findAll();
+
+  res.send(items);
+});
+
+router.get('/info', async (req, res) => {
+  const url: string = req.query.url as string;
+
+  if (url === '') res.status(400).send('Missing url parameter');
+
+  fetch(`https://ecommerce-scraper-saeh.onrender.com/?url=${url}`, { headers: { 'Content-type': 'application/json' } })
+    .then(async (response) => {
+      if (response.ok) {
+        const body = await response.json();
+        res.status(200).send(body);
+      } else {
+        const error = await response.json();
+        res.status(400).send(error.message);
+      }
+    }).catch((e) => {
+      console.log(e);
+    });
+});
+
+// posts
 router.post('/', async (req, res) => {
   const data = req.body;
 
@@ -18,12 +45,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
-  const items = await Item.findAll();
+// puts
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
 
-  res.send(items);
+  Item.update({ ...data }, { where: { id } })
+    .then((updatedCount) => {
+      if (updatedCount[0] > 0) res.sendStatus(200);
+      else res.sendStatus(404);
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
 });
 
+// deletes
 router.delete('/:id', async (req, res) => {
   const id = req.params.id;
 
